c2e7a5a35091ebc874fc853071156cc7c3ef9cd0
kangourouuu <bmkhanh436@gmail.com.com>
2025-11-11 21:28:52 +0700

    fix backend

--- apps/backend/dist/app/features/auth/strategies/access-token.strategy.d.ts
+++ apps/backend/dist/app/features/auth/strategies/access-token.strategy.d.ts
@@ -4,7 +4,7 @@ type JwtPayload = {
     sub: string;
     email: string;
 };
-declare const AccessTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithoutRequest] | [opt: import("passport-jwt").StrategyOptionsWithRequest]) => Strategy & {
+declare const AccessTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class AccessTokenStrategy extends AccessTokenStrategy_base {
--- apps/backend/dist/app/features/auth/strategies/refresh-token.strategy.d.ts
+++ apps/backend/dist/app/features/auth/strategies/refresh-token.strategy.d.ts
@@ -1,7 +1,7 @@
 import { Strategy } from 'passport-jwt';
 import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
-declare const RefreshTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithoutRequest] | [opt: import("passport-jwt").StrategyOptionsWithRequest]) => Strategy & {
+declare const RefreshTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class RefreshTokenStrategy extends RefreshTokenStrategy_base {
